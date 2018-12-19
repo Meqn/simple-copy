5b0f28e0c2d44527c7c29d454e32effc8dbef238
mengqing723 <mengqing723@gmail.com>
2018-12-19 19:38:22 +0800

    test: add test todo ...

--- test/index.js
+++ test/index.js
@@ -1,6 +1,18 @@
 const fs = require('fs')
 const assert = require('assert')
+const copy = require('../lib')
 
+/* 
 describe('copy', function() {
   // todo
-})
+}) */
+
+/* 
+copy('imgs', 'statics/', (err, files, index) => {
+  if (err) {
+    console.error(err)
+  }
+}) */
+
+
+console.log('test todo ...')
